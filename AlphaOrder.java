1ec0f2ead9e009a92b86e685b673adacab29084a
sans2600 <110605153+sans2600@users.noreply.github.com>
2022-08-08 23:41:04 +0530

    Update AlphaOrder.java

--- AlphaOrder.java
+++ AlphaOrder.java
@@ -1,7 +1,23 @@
 /*
- * 3) Initialize two character variables in a program and display the characters in alphabetical order.
-Example1) if the first character is 's' and second character is 'e' then the output should be e,s
-Example2) if the first character is 'a' and second character is 'e', then the output should be a,e
+ * 3)  1)Inserting a record
+ABC International School wants to computerize students details.
+The school maintains a database of students in MYSQL.
+The student table contains information related to students and is shown in the following student table structure.
+Column Name Type Constraint
+Rollno Number (4) Primary Key
+StudentName Varchar (20) Not Null
+Standard Varchar (2) Not Null
+Date_Of_Birth Date
+Fees Number (9,2)
+ When a new student joins the school, the student record is inserted in the student table.
+The valid student details are as follows:
+• Rollno: Valid value is a 4-digit number
+• StudentName: Valid value can contain maximum 20 letters in uppercase
+• Standard : Valid values are Roman Letters representing I to X(I, II, III, IV….IX, X)
+Display Student details
+Write the code to display details of all the students, if no roll no. is passed, while executing the main program.
+If while executing the main program, the roll no. is passed, then it should display the record of that particular student
+Write a Java program to insert some records to the table and display all the records from student table
 
  */
 
